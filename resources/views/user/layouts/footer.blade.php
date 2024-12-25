8ee0533e93fa5e2ddcbae84d78bdea169171be96
mailinh2603 <147016244+mailinh2603@users.noreply.github.com>
2024-12-26 00:20:43 +0700

    Update footer.blade.php

--- resources/views/user/layouts/footer.blade.php
+++ resources/views/user/layouts/footer.blade.php
@@ -3,7 +3,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>&copy; {{date('Y')}} Developed By Prajwal R.</span>
+            <span>&copy; {{date('Y')}} Phát triển bởi nhóm 5</span>
           </div>
         </div>
       </footer>
@@ -25,15 +25,15 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Rời khỏi?</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-body">Chọn "Đăng xuất" bên dưới nếu bạn sẵn sàng kết thúc phiên hiện tại của mình.</div>
         <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Hủy</button>
+          <a class="btn btn-primary" href="login.html">Đăng xuất</a>
         </div>
       </div>
     </div>
